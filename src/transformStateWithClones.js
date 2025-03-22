f02673549c8184b0629ab81a652c983bccec19a0
sirserg <sirserg05@gmail.com>
2025-03-22 19:40:12 +0200

    feat: add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  for (const action of actions) {
+    const newState = result[result.length - 1]
+      ? { ...result[result.length - 1] }
+      : { ...state };
+
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    } else {
+      for (const key in newState) {
+        delete newState[key];
+      }
+    }
+
+    result.push(newState);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
